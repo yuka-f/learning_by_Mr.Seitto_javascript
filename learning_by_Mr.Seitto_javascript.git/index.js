90c61bcd852497984b5af3ce58a95c7bc50b7118
yuka fujii <batonyf@icloud.com>
2020-12-08 10:42:58 +0900

    CreatObject

--- learning_by_Mr.Seitto_javascript.git/index.js
+++ learning_by_Mr.Seitto_javascript.git/index.js
@@ -17,15 +17,47 @@ let inoki = ['いーち','にー','さ-ん','ダーー！！'];
 // }
 
 // 5 if / else もし〜なら〜する。という仮定法
-if(inoki.length > 10){
-  console.log('ボンバイエ！');
-} else {
-  console.log('ボンバ...!');
-}
+// if(inoki.length > 10){
+//   console.log('ボンバイエ！');
+// } else {
+//   console.log('ボンバ...!');
+// }
 
 // ６　関数　使用方法は２パターンある
 // ・同じ命令を何度も使いたいとき
 // ・任意のタイミングで命令を実行したい時
-const test = () => {
-
+const test = (arg) => {
+  if(inoki.length > arg){
+    console.log('ボンバイエ！');
+  } else {
+    console.log('ボンバ...!');
+  }
 };
+
+
+
+// オブジェクト　変数に比べて情報を入れれる
+const unko2 = {
+  color: 'pink',
+  size: 'latge',
+  purfume: 'mint',
+  goToilet: () => {
+    console.log('Hello world!')
+  }
+};
+
+// 特殊な最初からあるオブジェクト　
+// ・window 画面のプロパティを取ってくる事ができる
+    // console.log(window.出したいプロパティ)
+
+// ・window.alert ポップアップを呼ぶ関数
+    // window.alert('出したいポップアップ');
+    
+// ・document　タグやページに何かしたい時に使う
+    //  document.getElementsByTagName('')
+    // getElementsByTagName:HtMLの何かを指定・参照したい時に使う
+
+// ・event ユーザーのアクションによって起こしたい時に使う。
+    // document.getElementsByTagName('button')[0].addEventListener('イヴェントタイプ',()=>{命令を書く});
+    // addEventListener:引数を２つ指定できる
+
