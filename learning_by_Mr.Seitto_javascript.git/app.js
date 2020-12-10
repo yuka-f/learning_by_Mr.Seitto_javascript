b210d81e23cb1ee200839dd12361c83f48b0bf89
yuka fujii <batonyf@icloud.com>
2020-12-10 18:36:28 +0900

    Creat app.js

---
+++ learning_by_Mr.Seitto_javascript.git/app.js
@@ -0,0 +1,11 @@
+const puestion = '私がエンジニアになりたい理由はどれでしょう？';
+const ansers = [
+  '給料あがいいから',
+  'スキルを身につけて将来フリーランスになりtらいから',
+  '好奇心と発想力を活かしたいから',
+  '父に言われたから',
+  '日本で最近流行っているから'
+];
+const correct = '好奇心と発想力を活かしたいから';
+
+console.log(document.getElementByID('js-question'));
