d18ce6c4d67af81cf81f8bf387ad198366ebd374
yuka fujii <batonyf@icloud.com>
2020-12-14 12:37:15 +0900

    Creat button

--- learning_by_Mr.Seitto_javascript.git/app.js
+++ learning_by_Mr.Seitto_javascript.git/app.js
@@ -9,16 +9,49 @@ const correct = '好奇心と発想力を活かしたいから';
 
 // 定数の文字列をHTMLに反映させる
 document.getElementById('js-question').textContent = question;
-document.getElementsByTagName('button')[0].textContent = answers[0];
-document.getElementsByTagName('button')[1].textContent = answers[1];
-document.getElementsByTagName('button')[2].textContent = answers[2];
-document.getElementsByTagName('button')[3].textContent = answers[3];
+
+const $button = document.getElementsByTagName('button')
+
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+$button[3].textContent = answers[3];
+
+let buttonIndex = 0;
+let buttonLength = $button.length;
+while(buttonIndex < buttonLength){
+  // ここに命令
+  buttonIndex++;
+}
 
 // ボタンをクリックしたら正誤判定
-document.getElementsByTagName('button')[0].addEventListener('click', () => {
-  if(){
-    window.alert('当たり！')
+$button[0].addEventListener('click', () => {
+  if(correct === $button[0].textContent){
+    window.alert('当たり！');
   } else {
-    window.alert('残念！')
+    window.alert('残念！');
+  }
+});
+$button[1].addEventListener('click', () => {
+  if(correct === $button[1].textContent){
+    window.alert('当たり！');
+  } else {
+    window.alert('残念！');
+  }
+});
+
+$button[2].addEventListener('click', () => {
+  if(correct === $button[2].textContent){
+    window.alert('当たり！');
+  } else {
+    window.alert('残念！');
+  }
+});
+
+$button[3].addEventListener('click', () => {
+  if(correct === $button[3].textContent){
+    window.alert('当たり！');
+  } else {
+    window.alert('残念！');
   }
 });
