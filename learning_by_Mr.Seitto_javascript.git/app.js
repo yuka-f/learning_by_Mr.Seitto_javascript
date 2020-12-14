96f37d66405a6174e13c6b9d9132a569aac8ab2c
yuka fujii <batonyf@icloud.com>
2020-12-15 00:59:03 +0900

    regactaring

--- learning_by_Mr.Seitto_javascript.git/app.js
+++ learning_by_Mr.Seitto_javascript.git/app.js
@@ -1,57 +1,45 @@
 const question = '私がエンジニアになりたい理由はどれでしょう？';
 const answers = [
   '給料がいいから',
-  'スキルを身につけて将来フリーランスになりtらいから',
+  'スキルを身につけて将来フリーランスになりたいから',
   '好奇心と発想力を活かしたいから',
   '父に言われたから',
 ];
 const correct = '好奇心と発想力を活かしたいから';
 
-// 定数の文字列をHTMLに反映させる
-document.getElementById('js-question').textContent = question;
-
 const $button = document.getElementsByTagName('button')
 
-$button[0].textContent = answers[0];
-$button[1].textContent = answers[1];
-$button[2].textContent = answers[2];
-$button[3].textContent = answers[3];
-
-let buttonIndex = 0;
-let buttonLength = $button.length;
-while(buttonIndex < buttonLength){
-  // ここに命令
-  buttonIndex++;
+// クイズの問題文、選択肢を定義(まとまりごとに関数に入れるとみやすくなる)
+const setupQuiz = () => {
+  document.getElementById('js-question').textContent = question;
+  let buttonIndex = 0;
+  let buttonLength = $button.length;
+  while(buttonIndex < buttonLength){
+    $button[buttonIndex].textContent = answers[buttonIndex];
+    buttonIndex++;
+  }
 }
+setupQuiz();
+
+const clickHandler = () => {
+  if(correct === e.target.textContent){
+    window.alert('当たり！');
+  } else {
+    window.alert('残念！');
+  }
+};
+
 
 // ボタンをクリックしたら正誤判定
-$button[0].addEventListener('click', () => {
-  if(correct === $button[0].textContent){
-    window.alert('当たり！');
-  } else {
-    window.alert('残念！');
-  }
+$button[0].addEventListener('click', (e) => {
+  clickHandler();
 });
-$button[1].addEventListener('click', () => {
-  if(correct === $button[1].textContent){
-    window.alert('当たり！');
-  } else {
-    window.alert('残念！');
-  }
+$button[1].addEventListener('click', (e) => {
+  clickHandler();
 });
-
-$button[2].addEventListener('click', () => {
-  if(correct === $button[2].textContent){
-    window.alert('当たり！');
-  } else {
-    window.alert('残念！');
-  }
+$button[2].addEventListener('click', (e) => {
+  clickHandler();
 });
-
-$button[3].addEventListener('click', () => {
-  if(correct === $button[3].textContent){
-    window.alert('当たり！');
-  } else {
-    window.alert('残念！');
-  }
+$button[3].addEventListener('click', (e) => {
+  clickHandler();
 });
