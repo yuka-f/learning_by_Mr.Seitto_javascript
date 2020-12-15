b36cdbedc5b1aaa3ef41d1b4e14f80de1807114a
yuka fujii <batonyf@icloud.com>
2020-12-15 09:51:09 +0900

    リファクタリングquiz

--- learning_by_Mr.Seitto_javascript.git/app.js
+++ learning_by_Mr.Seitto_javascript.git/app.js
@@ -1,11 +1,44 @@
-const question = '私がエンジニアになりたい理由はどれでしょう？';
-const answers = [
-  '給料がいいから',
-  'スキルを身につけて将来フリーランスになりたいから',
-  '好奇心と発想力を活かしたいから',
-  '父に言われたから',
+const quiz = [
+  {
+    question: '私がエンジニアになりたい理由はどれでしょう？',
+    answers: [
+      '給料がいいから',
+      'スキルを身につけて将来フリーランスになりたいから',
+      '好奇心と発想力を活かしたいから',
+      '父に言われたから',
+    ],
+    correct: '好奇心と発想力を活かしたいから'
+  },{
+    question: '私の前職の職種は何でしょう？',
+    answers: [
+      'エステティック部員',
+      '美容師',
+      'アパレル店員',
+      '留学していた',
+    ],
+    correct: 'エステティック部員'
+  },{
+    question: '私が最も転職の軸にしていることは何でしょう？',
+    answers: [
+      '休日やお給料、残業手当',
+      'スキルアップできる環境勉強会などがあるかどうか',
+      'お昼寝ができるかどうか',
+      '考え方',
+    ],
+    correct: '考え方'
+  },{
+    question: '私が周りから二番目に言われることは何でしょう？',
+    answers: [
+      '発想が斬新',
+      '危なっかしい',
+      '周りの状況をよくみてる',
+      'レアもん',
+    ],
+    correct: '危なっかしい'
+  }
 ];
-const correct = '好奇心と発想力を活かしたいから';
+const quizLength = quiz.length;
+let quizIndex = 0;
 
 const $button = document.getElementsByTagName('button')
 const buttonLength = $button.length;
