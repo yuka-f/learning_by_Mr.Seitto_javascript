b3162b59477878a26fbbc0a5b141783115eeecbe
yuka fujii <batonyf@icloud.com>
2020-12-24 21:14:16 +0900

    creatTab.js

---
+++ learning_by_Mr.Seitto_javascript.git/tab.js
@@ -0,0 +1,10 @@
+(()=>{
+
+    const $doc = document;
+    const $tab = $doc.getElementById('js-tab');
+    const $nav = $tab.querySelectorAll('[data-nav]');
+    const $content = $tab.querySelectorAll('[data-content]')
+
+    console.log('save', $nav);
+
+})();
